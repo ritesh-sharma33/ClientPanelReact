eed6c278c98a1315b58cebc2f14612f734cfec14
Ritesh Sharma <sharmaritesh3312@gmail.com>
2020-02-09 23:06:35 +0530

    Initial app setup and basic layouts done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import AppNavbar from './components/layout/AppNavbar';
 import Dashboard from './components/layout/Dashboard';
 
